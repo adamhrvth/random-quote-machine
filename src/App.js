7bb65527e0b55cfaf52896ea6798929adfa4fdce
Adam Horvath <horadam0207@gmail.com>
2022-11-17 17:34:55 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
 import { useState, useEffect} from "react";
 import "./App.css";
 
+const RANDOM_QUOTE_API = "http://api.quotable.io/random";
 
 const App = () => {
 
-
-  const [quoteData, setQuoteData] = useState({content: "", author: ""});
+  const [quoteData = getRandomQuote(), setQuoteData] = useState({content: "", author: ""});
   const [currentColor, setCurrentColor] = useState("#000000");
 
+  // load an initial quote
   useEffect(() => {
     getRandomQuote();
   }, []);
@@ -19,44 +19,43 @@ const App = () => {
 
 
   async function getRandomQuote() {
-    const response = await fetch("http://api.quotable.io/random");
+    const response = await fetch(RANDOM_QUOTE_API);
     const quoteObj = await response.json();
     setQuoteData(quoteObj);
   };
 
   function getRandomColor() {
-    const letters = '0123456789ABCDEF';
+    const hexLetters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += hexLetters[Math.floor(Math.random() * 16)];
     }
     setCurrentColor(color);
   }
   
-  const textColorStyle = {
+  const currentTextColorStyle = {
       color: currentColor,
       transition: "all 1.5s ease"
   };
 
-  const backgroundColorStyle = {
+  const currentBackgroundColorStyle = {
     backgroundColor: currentColor,
     transition: "all 1.5s ease"
   };
 
   return(
-    <div id = "wrapper" style = {backgroundColorStyle}>
+    <div id = "wrapper" style = {currentBackgroundColorStyle}>
       <div id="quote-box">
-        <p id="text" style={textColorStyle}>{'"' +quoteData.content + '"'}</p>
-        <p id="author" style={textColorStyle}>{quoteData.author}</p>
+        <p id="text" style={currentTextColorStyle}>{'"' +quoteData.content + '"'}</p>
+        <p id="author" style={currentTextColorStyle}>{quoteData.author}</p>
         <button 
           id="new-quote"
           onClick = {() => getRandomQuote()}
-          style={backgroundColorStyle}
+          style={currentBackgroundColorStyle}
           >
             New quote
         </button>
       </div>
-      <p id="developer-sign">by <a href="http://adamhrvth.dev">Adam Horvath</a></p>
     </div>
   );
 
